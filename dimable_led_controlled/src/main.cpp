0c34e40db882a0eb646f2b4748b1eed0277530d1
Siarhei Lunski <loonskai@gmail.com>
2022-06-16 13:08:10 +0200

    add buzzer :)

--- dimable_led_controlled/src/main.cpp
+++ dimable_led_controlled/src/main.cpp
@@ -2,6 +2,9 @@
 
 int ledPin = 6;
 
+int buzzPin = 5;
+int buzzDelay = 200;
+
 int decButtonPin = 11;
 int decButtonOld;
 int decButtonNew;
@@ -12,13 +15,14 @@ int incButtonNew;
 int currStep;
 
 int MIN_STEP = 0;
-int MAX_STEP = 5;
+int MAX_STEP = 10;
 int COEF = 255 / MAX_STEP;
 
 void setup()
 {
   pinMode(decButtonPin, INPUT);
   pinMode(incButtonPin, INPUT);
+  pinMode(buzzPin, OUTPUT);
   pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
 }
@@ -49,11 +53,18 @@ void readDec()
   decButtonOld = decButtonNew;
 }
 
+void buzz()
+{
+  digitalWrite(buzzPin, HIGH);
+  delayMicroseconds(buzzDelay * currStep);
+  digitalWrite(buzzPin, LOW);
+  delayMicroseconds(buzzDelay * currStep);
+}
+
 void loop()
 {
   readInc();
   readDec();
+  buzz();
   analogWrite(ledPin, currStep * COEF);
-  Serial.println(currStep * COEF);
-  delay(100);
 }
